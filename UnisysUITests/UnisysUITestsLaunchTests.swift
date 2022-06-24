d7482667617b5479cdff96336550d0f7b0077a79
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2022-06-24 15:26:21 +0200

    Initial Commit

---
+++ UnisysUITests/UnisysUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  UnisysUITestsLaunchTests.swift
+//  UnisysUITests
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2022 Oscar R. Garrucho. All rights reserved.
+//
+
+import XCTest
+
+class UnisysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
